c81ba21bc3442386b504ded8dd0a200910660978
fnavizq355 <132046114+fnavizq355@users.noreply.github.com>
2023-05-03 14:32:41 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,41 +1,80 @@
 function mostrar () {
-    patron.push(opciones._pickRandom())
+    acción = opciones._pickRandom()
+    patron.push(acción)
     for (let value of patron) {
-        if (value == "I") {
-            basic.showArrow(ArrowNames.West)
-        } else if (value == "D") {
-            basic.showArrow(ArrowNames.East)
-        } else {
-            basic.showString(value)
-        }
+        basic.showString("" + (value))
         basic.pause(100)
     }
 }
 input.onButtonPressed(Button.A, function () {
-	
+    if (mueve) {
+        acción = patron[n]
+        if (acción == "A") {
+            n = n + 1
+        } else {
+            juega = false
+            basic.showIcon(IconNames.No)
+        }
+    }
+})
+input.onGesture(Gesture.TiltRight, function () {
+    if (mueve) {
+        acción = patron[n]
+        if (acción == "D") {
+            n = n + 1
+        } else {
+            juega = false
+            basic.showIcon(IconNames.No)
+        }
+    }
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    patron.push("I")
+    if (mueve) {
+        acción = patron[n]
+        if (acción == "I") {
+            n = n + 1
+        } else {
+            juega = false
+            basic.showIcon(IconNames.No)
+        }
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     patron = []
-    jugar = true
-    mostrar()
-    basic.pause(500)
-    mostrar()
-    basic.pause(500)
+    juega = true
+    mueve = false
     mostrar()
 })
+input.onButtonPressed(Button.B, function () {
+    if (mueve) {
+        acción = patron[n]
+        if (acción == "B") {
+            n = n + 1
+        } else {
+            juega = false
+            basic.showIcon(IconNames.No)
+        }
+    }
+})
+function mover () {
+    mueve = true
+    n = 0
+    while (juega && false) {
+    	
+    }
+    mueve = false
+}
+let n = 0
 let patron: string[] = []
+let acción = ""
 let opciones: string[] = []
-let jugar = false
-jugar = false
+let mueve = false
+let juega = false
+juega = false
+mueve = false
 opciones = [
 "A",
 "B",
 "I",
-"I"
+"D"
 ]
-basic.forever(function () {
-	
-})
